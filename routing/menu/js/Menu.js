39b2e77b670ea164ef22708b39af8d9c441da5c2
Ilya Kyazimov <40570012+BruceWayne3@users.noreply.github.com>
2020-08-06 14:28:03 +0300

    Add files via upload

---
+++ routing/menu/js/Menu.js
@@ -0,0 +1,16 @@
+const Menu = () => (
+  <nav className="menu">
+    <NavLink to="/" className="menu__item" exact activeClassName={"menu__item-active"}>
+      Главная
+    </NavLink>
+    <NavLink to="/drift" className="menu__item" activeClassName={"menu__item-active"}>
+      Дрифт-такси
+    </NavLink>
+    <NavLink to="/timeattack" className="menu__item" activeClassName={"menu__item-active"}>
+      Time Attack
+    </NavLink>
+    <NavLink to="/forza" className="menu__item" activeClassName={"menu__item-active"}>
+      Forza Karting
+    </NavLink>
+  </nav>
+)
